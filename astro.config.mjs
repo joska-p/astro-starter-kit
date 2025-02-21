2bacdaa664a178de4693013f7f28be1ac71fb434
joska <jpotin@gmail.com>
2025-02-21 19:45:21 +0100

    use env variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 // @ts-check
+import process from "process";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+
+const { SITE_URL, BASE_URL } = loadEnv(
+  process.env.NODE_ENV || "development",
+  process.cwd(),
+  "",
+);
 
 const ReactCompilerConfig = {
   target: "19",
@@ -9,8 +17,8 @@ const ReactCompilerConfig = {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://joska-p.github.io/astro-starter-kit/",
-  base: "astro-starter-kit/",
+  site: SITE_URL,
+  base: BASE_URL,
   integrations: [
     react({
       babel: {
